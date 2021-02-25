ea9950ed45cff717532a63ceea719710724935c5
Yihao Liu <to.gnu.is.not.unix@gmail.com>
2021-02-25 16:32:59 +0800

    stable version

--- mces_wrapper/autogen.py
+++ mces_wrapper/autogen.py
@@ -18,13 +18,36 @@ class Func:
     """
     def __init__(self):
         self.func_name = None
-        self.arg_types = list()
-        self.arg_names = list()
-        self.arg_pointer_flags = list()
-        self.arg_inout_list = list()
         self.ret_type = None
         self.header_file = None
+        self.parameters = list()
 
+    def get_arg_names(self):
+        ret = list()
+        for param in self.parameters:
+            if param.arg_pointer_flag:
+                ret.append(param.arg_name + '_p')
+            else:
+                ret.append(param.arg_name)
+        return ', '.join(ret)
+
+
+class Param:
+    """
+    A class recording the information of the parameter of a C function
+    """
+    def __init__(self, param_info=(None, None, None)):
+        self.arg_pointer_flag = False
+        arg_info = list()
+        for info in param_info:
+            if info and ('*' in info or '[' in info):
+                self.arg_pointer_flag = True
+                info = re.sub(r'[\[\]*]', '', info)
+            arg_info.append(info)
+
+        self.arg_inout = arg_info[0]       # IN/ OUT
+        self.arg_type = arg_info[1]
+        self.arg_name = arg_info[2]
 
 
 class _Enum:
@@ -36,7 +59,6 @@ class _Enum:
         self.enum_name = None
         self.enum_members = list()
         self.enum_values = list()
-
 
 class EnumParser:
     """
@@ -121,19 +143,13 @@ class TypeDict:
 
     def load_enum_class_list(self):
         with open('enum_class.py') as fp:
-            lines = fp.readlines()
-
-        for line in lines:
-            if re.match(r'class .*\(Enum\):', line):
-                self.enum_class_list.append(line[6:-8])
+            content = fp.read()
+            self.enum_class_list = re.findall(r'class ([\w]+)[(]Enum[)]', content)
 
     def load_structure_class_list(self):
         with open('structure_class.py') as fp:
-            lines = fp.readlines()
-
-        for line in lines:
-            if re.match(r'class .*\(Structure\):', line):
-                self.structure_class_list.append(line[6:-13])
+            content = fp.read()
+            self.structure_class_list = re.findall(r'class ([\w]+)[(]Structure[)]', content)
 
 
 class HeaderParser:
@@ -157,39 +173,26 @@ class HeaderParser:
         for h_file in self.h_files:
             with open(h_file) as fp:
                 contents = fp.read()
-                contents = contents.split('MCESD_STATUS ')
-                contents.pop(0)
+                contents = re.sub(r'/\*.*\*/', '', contents)  # remove comments
+                contents = re.findall(r'MCESD_FUNC ([*\w]+) ([\w]+)([^;]+)', contents)  # find all functions
+                # For each function
                 for content in contents:
                     func = Func()
-                    func_info = content.split(');')[0]
-                    func_name = func_info.split('(')[0].replace('\n', '')
-                    func_args = func_info.split('(')[1].replace('\n', '')
-                    func_args = func_args.split(',')
+                    ret_type = content[0]
+                    if self.type_dict.basic_type_dict.get(ret_type, 0):
+                        func.ret_type = self.type_dict.basic_type_dict[ret_type]
+                    else:
+                        logging.error(f'Unrecognized ret type {ret_type}')
+                    func.ret_type = ret_type
+                    func.func_name = content[1]
+                    param_infos = re.sub(r'[\n()]', '', content[2])                          # remove () and \n in parameters
+                    param_infos = param_infos.split(',')
+                    for param_info in param_infos:
+                        param_info = re.search(r'(IN|OUT)\s+([\[\]*\w\s]+)\s+([\[\]*\w]+)', param_info).groups()
+                        param = Param(param_info=param_info)
 
-                    func.func_name = func_name
-                    func_arg_stripped = [func_arg.strip() for func_arg in func_args]
-                    for func_arg in func_arg_stripped:
-                        params = func_arg.split()
-                        arg_name = params.pop()
-                        if '*' in arg_name:
-                            arg_name = arg_name[1:]
-                            func.arg_pointer_flags.append(1)
-                        elif '[]' in arg_name:
-                            arg_name = arg_name[:-2]
-                            func.arg_pointer_flags.append(1)
-                        elif '[ ]' in arg_name:
-                            arg_name = arg_name[:-3]
-                            func.arg_pointer_flags.append(1)
-                        else:
-                            func.arg_pointer_flags.append(0)
-
-                        arg_inout = params.pop(0)
-
-                        if len(params) > 1:
-                            arg_type = ' '.join(params)
-                        else:
-                            arg_type = params[0]
-
+                        # convert customized variable type to ctype according to the type dict
+                        arg_type = param.arg_type
                         if self.type_dict.basic_type_dict.get(arg_type, 0):
                             arg_type = self.type_dict.basic_type_dict[arg_type]
                         elif arg_type in self.type_dict.enum_class_list:
@@ -199,49 +202,54 @@ class HeaderParser:
                         elif self.type_dict.special_type_dict.get(arg_type, 0):
                             if arg_type == 'MCESD_FIELD_PTR':
                                 arg_type = 'MCESD_FIELD'
-                                func.arg_pointer_flags.append(1)
+                                param.arg_pointer_flag = True
                             else:
                                 arg_type = self.type_dict.special_type_dict[arg_type]
                         else:
-                            logging.warning(f'No valid type: {arg_type}； func_name: {func_name}')
+                            logging.warning(f'No valid type: {arg_type}； func_name: {func.func_name}')
+                        param.arg_type = arg_type
+                        func.parameters.append(param)
 
-                        func.arg_inout_list.append(arg_inout)
-                        func.arg_types.append(arg_type)
-                        func.arg_names.append(arg_name)
-                        func.ret_type = 'c_uint32'
-                        func.header_file = os.path.basename(h_file)
+                    # TODO: ret_type check in type dict
+                    func.ret_type = 'c_uint32'
+                    func.header_file = os.path.basename(h_file)
 
                     self.func_list.append(func)
 
     def write_funcs_to_wrapper(self):
         # initialize the wrapper
         shutil.copyfile('structure_class.py', self.wrapper)
+        with open(self.wrapper, 'a') as fp:
+            fp.write('\nMZDAPILib = CDLL("..\Debug\MZD.dll")\n\n')
 
         for func in self.func_list:
             with open(self.wrapper, 'a') as fp:
-                arg_names = ', '.join(func.arg_names)
+
+                arg_names = func.get_arg_names()
                 fp.write(f'def {func.func_name}({arg_names}):\n')
 
                 # Comment of this function
                 fp.write('    """\n')
                 arg_types = list()
-                for arg_type, arg_name, flag in zip(func.arg_types, func.arg_names, func.arg_pointer_flags):
+                for param in func.parameters:
+                    arg_type = param.arg_type
+                    arg_name = param.arg_name
                     if arg_type in self.type_dict.enum_class_list:
-                        if flag:
+                        if param.arg_pointer_flag:
                             arg_types.append('POINTER(c_int)')
                             fp.write(f'    :param {arg_name}: A pointer of the enumerate class {arg_type}\n')
                         else:
                             arg_types.append('c_int')
                             fp.write(f'    :param {arg_name}: member from enumerate class {arg_type}\n')
                     elif arg_type in self.type_dict.structure_class_list:
-                        if flag:
+                        if param.arg_pointer_flag:
                             arg_types.append(f'POINTER({arg_type})')
                             fp.write(f'    :param {arg_name}: A pointer of the structure class {arg_type}\n')
                         else:
                             arg_types.append(arg_type)
                             fp.write(f'    :param {arg_name}: implementation of the structure class {arg_type}\n')
                     else:
-                        if flag:
+                        if param.arg_pointer_flag:
                             arg_types.append(f'POINTER({arg_type})')
                             fp.write(f'    :param {arg_name}: A pointer of {arg_type}\n')
                         else:
@@ -268,19 +276,21 @@ class HeaderParser:
                 fp.write(f'    logging.info("Function name : {func.func_name}")\n')
                 arg_names = list()
                 logging_infos = list()
-                for arg_type, arg_name, flag in zip(func.arg_types, func.arg_names, func.arg_pointer_flags):
+                for param in func.parameters:
+                    arg_type = param.arg_type
+                    arg_name = param.arg_name
                     # common param
                     if arg_type in basic_type_list:
-                        fp.write(f'    {arg_name} = 1\n')       # maybe we could iterate/ lane ranges from 0, 1, 2, 3
+                        fp.write(f'    {arg_name} = 1\n')  # maybe we could iterate/ lane ranges from 0, 1, 2, 3
                         logging_infos.append(f'    logging.debug(f"{arg_name}' + ' = {' + f'{arg_name}' + '}")\n')
-                        if flag:
+                        if param.arg_pointer_flag:
                             fp.write(f'    {arg_name}_p = {arg_type}({arg_name})\n')
                             arg_name = arg_name + '_p'
                     elif arg_type in self.type_dict.enum_class_list:
                         member_names = eval(arg_type).__dict__['_member_names_']
-                        fp.write(f'    {arg_name} = {arg_type}.{member_names[0]}.value\n')    # maybe we could iterate
+                        fp.write(f'    {arg_name} = {arg_type}.{member_names[0]}.value\n')  # maybe we could iterate
                         logging_infos.append(f'    logging.debug(f"{arg_name}' + ' = {' + f'{arg_name}' + '}")\n')
-                        if flag:
+                        if param.arg_pointer_flag:
                             fp.write(f'    {arg_name}_p = c_long({arg_name})\n')
                             arg_name = arg_name + '_p'
                     elif arg_type in self.type_dict.structure_class_list:
@@ -290,13 +300,13 @@ class HeaderParser:
                             attr_name = attr[0]
                             attr_class = attr[1]
                             if attr_class == c_bool:
-                                attr_inits.append('0')            # 0 or 1
+                                attr_inits.append('0')  # 0 or 1
                             else:
-                                attr_inits.append('0')            # Just give it an initial value... Usually it is used for OUT
+                                attr_inits.append('0')  # Just give it an initial value... Usually it is used for OUT
                         attr_init = ', '.join(attr_inits)
                         fp.write(f'    {arg_name} = {arg_type}({attr_init})\n')
                         logging_infos.append(f'    logging.debug(f"{arg_name}' + ' = {' + f'{arg_type}.{attr_name}' + '}")\n')
-                        if flag:
+                        if param.arg_pointer_flag:
                             fp.write(f'    {arg_name}_p = byref({arg_name})\n')
                             arg_name = arg_name + '_p'
                     elif arg_name == 'devPtr':
@@ -326,11 +336,6 @@ if __name__ == '__main__':
     enum_parser = EnumParser(h_files)
     h_files = glob.glob(os.path.join(os.path.join('..', 'C112GX4'), '*.h'))
     head_parser = HeaderParser(h_files)
-    # head_parser.parse()
-    # head_parser.write_funcs_to_wrapper()
-    # head_parser.write_testcase()
-
-
-
-
-
+    head_parser.parse()
+    head_parser.write_funcs_to_wrapper()
+    head_parser.write_testcase()
